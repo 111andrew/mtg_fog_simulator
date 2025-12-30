8cf4ce5c99c7359add055e7d66d30f13f1b19f96
Andrew Chu <46828944+111andrew@users.noreply.github.com>
2025-12-30 10:17:01 -0600

    Create enchantment_class.py

---
+++ card_classes/enchantment_class.py
@@ -0,0 +1,11 @@
+import card_classes.card_class as card
+
+class JacesErasure(card.Card):
+    def __init__(self):
+        super().__init__('JacesErasure', 'enchantment', ['win con'], 2, 0, 1, 0, 0, 0, 1)
+        self.tapped = False
+
+    def play(self, mtg_play):
+        mtg_play.play_permanent(self.get_name())
+        # set ability into status
+        mtg_play.status.append('JacesErasure')
